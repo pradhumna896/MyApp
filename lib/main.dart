d0744ead3239e35ea9f0947117225d71f89ba82b
pradhumna <pradhumna896@gmail.com>
2022-03-24 22:55:43 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import './screens/product_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.purple,
+        accentColor:Colors.deepOrange,
+      ),
+      home:  ProducrOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (ctx)=>ProductDetailScreen(),
+      },
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+
+      ),
+      
+    );
+  }
+}
+
---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+
+  // ProductDetailScreen(this.title);
+  static const routeName = '/product-detail';
+
+  // ignore: empty_constructor_bodies
+  @override
+  Widget build(BuildContext context) {
+  final productId = ModalRoute.of(context)!.settings.arguments as String;//is the id!
+    //...
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
+  }
+}
